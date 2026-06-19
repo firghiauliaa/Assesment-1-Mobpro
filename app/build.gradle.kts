ecc268fff0fce6ea8fb702e6d78628b1e49cfe3f
Firghi Aulia Pahlevi <firghiauliaa@gmail.com>
2026-06-19 17:09:22 +0700

    Menambahkan depedency Retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+
 }
 
 android {
@@ -61,6 +62,9 @@ dependencies {
 
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
+
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
 
     ksp(libs.androidx.room.compiler)
 
---
+++ app/src/main/java/com/firghi0101/assesment1/network/FuelApiService.kt
@@ -0,0 +1,4 @@
+package com.firghi0101.assesment1.network
+
+class FuelApiService {
+}
